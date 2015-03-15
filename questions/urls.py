11418d11d07577fbf83970009a53d71a444de757
Taylor Sloan <cinebrahapp@gmail.com>
2015-03-15 00:53:10 -0700

    added rest framework shit

--- questions/urls.py
+++ questions/urls.py
@@ -1,10 +1,16 @@
 __author__ = 'Taylor'
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
+
+import views
 
 
 urlpatterns = patterns('',
     # Gets a random question
+    url(r'^$', views.get_random_question)
     # Provide answer to question
     # url(r'^(?P<pk>\d+)/positive$', views.),
     # url(r'^(?P<pk>\d+)/negative$', views.),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- questions/views.py
+++ questions/views.py
@@ -1,11 +1,34 @@
-from rest_framework import viewsets
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 from .models import Question
 from .serializers import QuestionSerializer
 
 
-class QuestionViewSet(viewsets.ModelViewSet):
+class QuestionDetail(APIView):
 
-    serializer_class = QuestionSerializer
-    queryset = Question.objects.all()
+    def get_object(self, pk):
+        try:
+            return Question.objects.get(pk=pk)
+        except Question.DoesNotExist:
+            raise Http404
 
+    def get(self, request, pk, format=None):
+        question = self.get_object(pk)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        question = self.get_object(pk)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view()
+def get_random_question(request, format=None):
+    question = Question(question='This is a test question')
+    # question.save()
+    serializer = QuestionSerializer(question)
+    return Response(serializer.data)
