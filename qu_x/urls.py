1292d26b7735fb723aa80c4d8a4f3b8ec0c13e5d
Taylor Sloan <cinebrahapp@gmail.com>
2015-03-15 00:37:01 -0700

    added rest framework shit

--- qu_x/urls.py
+++ qu_x/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+
+from questions import views
+
+router = DefaultRouter()
+router.register(r'questions', views.QuestionViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'qu_x.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('questions.urls')),
+    url(r'^$', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
